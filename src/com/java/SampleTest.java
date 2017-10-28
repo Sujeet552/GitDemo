4d5fc9ef4042ae83f941a395d2da47d6ba72bc52
Sujeet552 <mr.sujeet552@gmail.com>
2017-10-28 19:39:45 +0530

    Small changes in output

--- src/com/java/SampleTest.java
+++ src/com/java/SampleTest.java
@@ -5,7 +5,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 
 		System.out.println("This is my first demo2");
-		System.out.println("This is my first demo5");
+		System.out.println("This is my first demo6");
 	}
 
 }
